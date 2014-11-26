120c3629bca7d2877e16b2c8df809082031a4748
rodrigopavezi <rodrigopavezi@gmail.com>
2014-11-26 15:20:28 -0200

    Changed update to patch and fixed data parsing issue. parse the data as an json object instead of string.

--- javascript/endpoints/schedule/core.schedule.patch.js
+++ javascript/endpoints/schedule/core.schedule.patch.js
@@ -1,17 +1,17 @@
 /**
  * Created by rodrigopavezi on 11/14/14.
  */
-function updateCompany() {
-    console.log('updateCompany');
+function patchSchedule() {
+    console.log('patchSchedule');
     // getting parameter values
-    var _data = document.getElementById('updateCompany_data').value;
-    var _id = document.getElementById('updateCompany_id').value;
-    var _fields = document.getElementById('updateCompany_fields').value;
+    var _data = document.getElementById('patchSchedule_data').value;
+    var _id = document.getElementById('patchSchedule_id').value;
+    var _fields = document.getElementById('patchSchedule_fields').value;
 
     // create a javascript object which will be converted to Json
     var parameters = {};
     if (_data) {
-        parameters['data'] = _data;
+        parameters['data'] = JSON.parse(_data);
     }
     if (_id) {
         parameters['id'] = _id;
@@ -22,10 +22,10 @@ function updateCompany() {
 
     // loading and calling the api passing the parameter object
     gapi.client.load(API_NAME, API_VER, function () {
-        var request = gapi.client.core.company.update(parameters);
+        var request = gapi.client.core.schedule.patch(parameters);
 
         request.execute(function (jsonResp, rawResp) {
-            handlesResponse(jsonResp, rawResp, 'updateCompanyResult');
+            handlesResponse(jsonResp, rawResp, 'patchScheduleResult');
         });
     }, ROOT);
 }
